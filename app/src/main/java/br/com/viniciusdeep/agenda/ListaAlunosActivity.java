64a8285280a3acc4fb1989c5e7ca3e8e459e2375
Vinicius Deep <viniciuscorreia11@gmail.com>
2018-04-16 21:40:19 -0300

    Implement call phone, and permission on manifest

--- app/src/main/java/br/com/viniciusdeep/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/viniciusdeep/agenda/ListaAlunosActivity.java
@@ -1,11 +1,15 @@
 package br.com.viniciusdeep.agenda;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Browser;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.ContextMenu;
@@ -21,6 +25,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.List;
+
 import br.com.viniciusdeep.agenda.Dao.AlunoDao;
 import br.com.viniciusdeep.agenda.modelo.Aluno;
 
@@ -38,7 +43,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> list, View item, int position, long id) {
                 Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(position);
-                Intent intent = new Intent(ListaAlunosActivity.this,FormularioActivity.class );
+                Intent intent = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
                 intent.putExtra("aluno", aluno);//Enviar pra outra activity
                 startActivity(intent);
 
@@ -95,8 +100,34 @@ public class ListaAlunosActivity extends AppCompatActivity {
         String telefone = aluno.getTelefone();
         MenuItem itemSMS = menu.add("Enviar SMS");
         Intent intentSms = new Intent(Intent.ACTION_VIEW);
-        intentSms.setData(Uri.parse("sms:"+ telefone));
+        intentSms.setData(Uri.parse("sms:" + telefone));
         itemSMS.setIntent(intentSms);
+
+
+        MenuItem itemLigar = menu.add("Ligar para Aluno");
+        itemLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+
+
+
+                if (ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 123);
+
+
+                } else {
+                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:" + aluno.getTelefone()));
+                    startActivity(intentLigar);
+                }
+
+                return false;
+            }
+        });
+
+
+
 
 
         MenuItem itemEndereco = menu.add("Mostrar Endereço no Mapa ");
@@ -136,4 +167,12 @@ public class ListaAlunosActivity extends AppCompatActivity {
             }
         });
     }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//            if(requestCode == 123) {
+//
+//            }
+//    } Metódo para manipular permissões, o request code é para verificar qual é a permissão, e executar caso for ela
 }
